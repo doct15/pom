4c4cf57d47c19e4abc927ac7851a9783cc61a3bb
Brian McGehee <doc@tavian.com>
2018-01-13 15:27:50 -0800

    huh

--- sendandwait.sh
+++ sendandwait.sh
@@ -2,3 +2,4 @@
 #
 PASSWORD=$(cat /etc/pom.pwd)
 sendemail -f doc@tavian.com -t doc@tavian.com -u "POM" -m "$(pom $(date '+%Y%m%d08');pom $(date '+%Y%m%d20');echo "";echo "Your fortune...";fortune)" -s smtp.tavian.com:587 -xu distelli@tavian.com -xp $PASSWORD -v
+
