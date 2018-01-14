1f68ef2f82712fab90c7f3ec5cc2f2e93fb199fe
doct15 <doct15@yahoo.com>
2018-01-14 11:01:26 -0800

    update with path to app

--- sendandwait.sh
+++ sendandwait.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #
 PASSWORD=$(cat /etc/pom.pwd)
-sendemail -f doc@tavian.com -t doc@tavian.com -u "POM" -m "$(pom $(date '+%Y%m%d08');pom $(date '+%Y%m%d20');echo "";echo "Your fortune...";fortune)" -s smtp.tavian.com:587 -xu distelli@tavian.com -xp $PASSWORD -v
-
+sendemail -f doc@tavian.com -t doc@tavian.com -u "POM" -m "$(/usr/games/pom $(date '+%Y%m%d08');/usr/games/pom $(date '+%Y%m%d20');echo "";echo "Your fortune...";/usr/games/fortune)" -s smtp.tavian.com:587 -xu distelli@tavian.com -xp $PASSWORD -v
