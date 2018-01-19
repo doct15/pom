4f9aff1910de1eed783db77a58d9a6873112c35b
doct15 <doct15@yahoo.com>
2018-01-18 20:40:11 -0800

    Chaha

--- cherylhaha.sh
+++ cherylhaha.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 PASSWORD=$(cat /etc/pom.pwd)
-sendemail -f doc@tavian.com -t clpetermann66@gmail.com -u "Will You be a Millionaire today?" -m "No. You will not be a millionaire today. Sorry. Check again tomorrow." -s smtp.tavian.com:587 -xu distelli@tavian.com -xp $PASSWORD -v
+sendemail -f doc@tavian.com -t clpetermann66@gmail.com -u "Will You be a Millionaire today?" -m "If you buy a lottery ticket today, you may win!" -s smtp.tavian.com:587 -xu distelli@tavian.com -xp $PASSWORD -v
